9b07a561dc106ccee64cc9c7c7bfc9f6ea7de1a6
Robert Young <apple@Robs-MacBook-Pro.local>
2018-07-13 10:08:06 +0100

    Upgraded to ng6

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,6 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+import 'zone.js/dist/zone-error';
+
 export const environment = {
   production: false
 };
