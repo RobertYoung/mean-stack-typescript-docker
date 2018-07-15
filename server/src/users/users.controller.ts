431c20a61303fc417c8ad9dcca977aef28a3d8fe
Robert Young <apple@Robs-MacBook-Pro.local>
2018-07-15 12:36:44 +0100

    Fixed intellisense for the server spec files

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { User } from '../models';
+import { User } from './models';
 
 @Controller('users')
 export class UsersController {
-
   @Get()
   root(): Array<User> {
     return [<User>{ firstName: 'Rob' }];
   }
-
 }
